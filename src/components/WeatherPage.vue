89e85b81dcabc5fbe9896019459b32116ebb4f20
deem-zee <zimindmitri@gmail.com>
2022-07-17 22:17:12 +0300

    добавлены стили для min-width=768px

--- src/components/WeatherPage.vue
+++ src/components/WeatherPage.vue
@@ -1,31 +1,33 @@
 <template>
     <div id="component">
-        <h1>{{header}}</h1>
-        <div id="header">
-            <nav id="nav"></nav>
-            <div id="geo"><h2>{{city}}</h2></div>
-            <div id="search"></div>
-        </div>
+        <!-- <h1>{{header}}</h1> -->
         <div class="container">
-            <div id="icon"><img :src = "iconPath" alt=""></div>
-            <p class="description">{{description}}</p>
-            <div id="temp"> <img src="../assets/thermometer.png" alt=""> <p>{{tempSign}} {{currentTemp}}<span>℃</span></p></div>
-            <p id="feelsLike">Ощущается как: {{feelsLikeSign}} &#8451;</p>
+            <div id="geo"><h2>{{city}}</h2></div>
+            <div id="mainInfo">
+                <div id="temp"><p>{{tempSign}} {{currentTemp}}</p><span style="font-size:48px">&#xb0;</span></div>
+                 <div id="icon"><img :src= "iconPath" alt=""></div>
+            </div>
+            
+            <p id="description">{{description}}</p>
+            <p id="feelsLike">Ощущается как: {{feelsLikeSign}}<span>&#xb0;</span></p>
         </div>  
-        <div id="addInfo">  
-            <div id="pressure"> <img src="../assets/barometer.png" alt=""> <p>{{pascalToHg}} мм. рт. ст.</p></div>
-            <div  id="humidity"><img src="../assets/Hygrometer.png" alt=""><p> {{humidity}}%</p></div>
-            <div id="wind"> <img src="../assets/wind.png" alt="" > <p>{{wind}} м/c</p></div>
-        </div>
 
         <div id="soon">
             <div v-for="(day, idx) in futureWeather" :key="idx">
-                <p>{{day.dt_txt.split(' ')[1].slice(0, 5)}}</p>
+                <p class="hours">{{day.dt_txt.split(' ')[1].slice(0, 5)}}</p>
                 <div><img :src="require(`../assets/iconList/${day.weather[0].icon}.png`)" alt=""> </div>
-                <div class="soonTemp"><img src="../assets/thermometer.png" alt="" class="small">{{tempSign}}{{Math.round(day.main.temp)}}<span>℃</span></div>
+                <div class="soonTemp"><span>{{tempSign}}{{Math.round(day.main.temp)}}&#xb0;</span></div>
             </div>
             
         </div>
+
+        <div id="addInfo">  
+            <div id="pressure" class="addInfo-div"> <img src="../assets/barometer.png" alt=""> <p>{{pascalToHg}} мм. рт. ст.</p></div>
+            <div  id="humidity" class="addInfo-div"><img src="../assets/Hygrometer.png" alt=""><p> {{humidity}}%</p></div>
+            <div id="wind" class="addInfo-div"> <img src="../assets/wind.png" alt="" > <p>{{wind}} м/c</p></div>
+        </div>
+
+       
 
         <div id="sun">
             <div id="sunrise"><p>{{getSunrise}}</p><img src="../assets/sunrise.png" alt=""><p>Восход</p></div>
@@ -76,8 +78,6 @@ export default {
             return this.feelsLike;
         },
         tempSign() {
-            // if(this.currentTemp > 0) {return '+' + this.currentTemp;}
-            // return this.currentTemp;
             if(this.currentTemp > 0) {
                 return '+';
             }
@@ -92,17 +92,7 @@ export default {
         getSunset() {
             return this.sunset.toString().split(' ')[4];
         },
-        // setIconPath() {
-        //     for(let part of this.futureWeather) {
-        //         console.log(typeof part,1111);
-        //         // eslint-disable-next-line no-inner-declarations
-        //         function iconPath() {return "http://openweathermap.org/img/wn/" + part.weather[0].icon + "@2x.png";}
-        //         iconPath.apply(this.futureWeather.part, part.weather[0].icon)
-        //         console.log(part,222)
-        //     }
-        //     console.log(this.futureWeather.iconPath,33333333)
-        //     return this.futureWeather.iconPath
-        // }
+
     },
     created() {
         fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=31753a5ec52cae14dffa4cadc0a2b489&units=metric&lang=ru')
@@ -124,7 +114,7 @@ export default {
             for(let i = 1; i < 6; i++) {
                 this.futureWeather.push(data.list[i])
             }
-             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&appid=31753a5ec52cae14dffa4cadc0a2b489&units=metric&lang=ru`)
+            //  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&appid=31753a5ec52cae14dffa4cadc0a2b489&units=metric&lang=ru`)
             // .then(response => console.log(response.json()))
             // .then(result => console.log(result))
             }); 
@@ -135,167 +125,347 @@ export default {
 
 <style scoped>
 
-@import url('https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Comfortaa:wght@300;400;500;600&family=Ubuntu:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Comfortaa:wght@300;400;500;600&family=Inter&family=Noto+Sans+Display&family=Ubuntu:wght@300&display=swap');
 
 /* font-family: 'Be Vietnam Pro', sans-serif;
 font-family: 'Comfortaa', cursive;
 font-family: 'Ubuntu', sans-serif; */
+/* font-family: 'Noto Sans Display', sans-serif; */
 
+@media screen and (min-width: 320px) {
+    
+    #component {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background: rgb(8, 195, 227);
+        font-family: 'Inter', sans-serif;
+        color: white;
+        text-align: center;
+        padding: 20px;
+    }
+    #container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    #geo {
+        font-size: 1em;
+    }
+
+    #icon img {
+        transform: scale(1.5);
+    }
+
+    #mainInfo {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin: 0;
+    }
+
+    #mainInfo img {
+        position: relative;
+        top: -10px;
+        left: 15px;
+    }
+
+    #description {
+        text-transform: capitalize;
+        font-size: 2em;
+        font-weight: bold;
+        margin: 0;
+   }
+   #temp {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+   }
+   #temp img {
+        width: 80px;
+        height: 80px;
+   }
+   #temp p {
+        font-size: 48px;
+        font-weight: bold;
+        margin: 0;
+        align-self: center;
+
+   }
+
+   #temp > .celsius  {
+    content: '\2103';
+    font-family: 'Ubuntu', sans-serif;
+
+   }
+
+    #feelsLike {
+        font-size: 1.8em;
+    }
+
+    #addInfo {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-top: 1px solid white;
+        border-bottom: 1px solid white;
+    }
+
+    .addInfo-div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 10px 0;
+    }
+
+    .addInfo-div img {
+        width: 35px;
+        height: 35px;
+        transform: scale(0.8)
+    }
+
+    .addInfo-div p {
+        font-size: 1em;
+    }
+
+    #pressure img {
+        position: relative;
+        top: 5px;
+    }
+
+    #wind img {
+        position: relative;
+        top: 4px;
+    }
+
+    #soon {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-bottom: 1px solid white;
+        padding: 10px;
+    }
+
+    #soon div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #soon img {
+        width: 35px;
+        height: 35px;
+        align-self: center;
+        position: relative;
+        top: -10px;
+    }
+
+    
+
+    #soon .soonTemp {
+        display: flex;
+        flex-direction: row;
+    }
+
+    #soon span {
+        align-self: center;
+    }
+
+    #sun {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    #sun img {
+        width: 85px;
+        height: 85px;
+    }
+
+    #soon, #addInfo, #sun {
+        border: 1px solid white;
+        border-radius: 10px;
+        margin: 5px 0;
+    }
+
+}
+
+/* стили для планшетов min-width: 768px */
+
+@media screen and (min-width: 768px) {
 
 #component {
-    background: rgb(1, 148, 246);
-    color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background: rgb(8, 195, 227);
+        font-family: 'Inter', sans-serif;
+        color: white;
+        text-align: center;
+        padding: 10px;
+    }
+    #container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    #geo {
+        font-size: 2em;
+    }
+
+    #icon img {
+        transform: scale(2);
+    }
+
+    #mainInfo {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin: 0;
+    }
+
+    #mainInfo img {
+        position: relative;
+        top: -10px;
+        left: 15px;
+    }
+
+    #description {
+        text-transform: capitalize;
+        font-size: 2.2em;
+        font-weight: bold;
+        margin: 0;
+   }
+   #temp {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+   }
+   #temp img {
+        width: 80px;
+        height: 80px;
+   }
+   #temp p {
+        font-size: 48px;
+        font-weight: bold;
+        margin: 0;
+        align-self: center;
+
+   }
+
+   #temp > .celsius  {
+    content: '\2103';
     font-family: 'Ubuntu', sans-serif;
-    width: 768px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
+
+   }
+
+    #feelsLike {
+        font-size: 2.5em;
+    }
+
+    #addInfo {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-top: 1px solid white;
+        border-bottom: 1px solid white;
+    }
+
+    .addInfo-div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 10px 0;
+    }
+
+    .addInfo-div img {
+        width: 35px;
+        height: 35px;
+        transform: scale(1.5)
+    }
+
+    .addInfo-div p {
+        font-size: 2em;
+    }
+
+    #pressure img {
+        position: relative;
+        top: 5px;
+        top: 30px;
+        left: -15px;
+    }
+
+    #wind img {
+        position: relative;
+        top: 25px;
+        left: -10px;
+    }
+
+    #humidity img {
+        position: relative;
+        top: 25px;
+        left: -20px;
+    }
+
+    #soon {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-bottom: 1px solid white;
+        padding: 10px;
+    }
+
+    #soon div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #soon .hours {
+        font-size: 1.5em;
+    }
+
+    #soon img {
+        width: 35px;
+        height: 35px;
+        align-self: center;
+        transform: scale(2.5)
+    }
+
+    #soon .soonTemp {
+        display: flex;
+        flex-direction: row;
+        font-size: 2em;
+    }
+
+    #soon span {
+        align-self: center;
+    }
+
+    #sun {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        font-size: 1.5em;
+    }
+
+    #sun img {
+        width: 85px;
+        height: 85px;
+        transform: scale(1.5);
+        margin: 10px 0;
+    }
+
+    #soon, #addInfo, #sun {
+        border: 1px solid white;
+        border-radius: 10px;
+        margin: 5px 0;
+    }
+
+
 }
 
-h1 {
-    width: 100%;
-    align-self: center;
-}
+/* cтили для монитора пк min-width: 1280px */
+@media screen and (min-width: 1280px) {
 
-#icon img{
-    width: 150px;
-    height: 150px;
-}
-
-p {
-    font-size: 28px;
-    margin-bottom: 0;
-}
-
-#temp {
-    display: flex;
-    justify-content: center;
-}
-
-#temp img {
-    position: relative;
-    top: 5px;
-    width: 30px;
-    height: 30px;
-}
-
-#temp p {
-    margin-top: 0;
-}
-
-#temp span{
-    font-weight: normal;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-
-}
-
-
-.description {
-    margin-top: 0;
-    font-size: 38px;
-    margin-bottom: 10px
-}
-
-#feelsLike {
-    font-weight: normal;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-
-.citySearch {
-    width: 300px;
-    height: 40px;
-    border: 2px solid rgb(66, 0, 128);
-    border-radius: 10px;
-    box-sizing: border-box;
-}
-
-.srcBtn {
-    background: url("https://upload.wikimedia.org/wikipedia/commons/7/7e/Vector_search_icon.svg");
-    width: 40px;
-    height: 40px;
-    background-repeat: no-repeat;
-    background-position: center;
-    border-radius: 10px;
-    position: relative;
-    top: 12px;
-    border: 2px solid rgb(66, 0, 128);
-    margin-left: 10px;
-}
-
-#addInfo {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    padding-bottom: 50px;
-    margin-bottom: 30px;
-    border-bottom: 1px solid white;
-}
-
-#pressure {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
-}
-
-#humidity {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
-}
-
-#wind {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
-}
-
-#addInfo img{
-    position: relative;
-    top: 30px;
-    width: 30px;
-    height: 30px;
-    margin-right: 10px;
-}
-
-#sun {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    margin-bottom: 30px;
-    padding-top: 30px;
-    border-top: 1px solid white;
-}
-
-#sun img {
-    width: 100px;
-    height: 100px;
-}
-
-#soon {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: space-evenly;
-    margin-bottom: 30px;
-}
-
-#soon div{
-    display: flex;
-    flex-direction: column;
-}
-
-.small {
-    width: 35px;
-    height: 35px;
-}
-
-.soonTemp {
-    display: flex;
-    flex-direction: row !important;
-    font-size: 20px;
-    font-weight: normal;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 
